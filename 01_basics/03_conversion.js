4e0ff4c7f346ff29990a614dbdb9ef80f3a51309
Ajay Sharma <ajaysharma27600@gmail.com>
2023-09-25 10:26:21 +0000

    Learn some Basics of Javascript

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+/* ********conversion of score Number to String********** */
+
+// let score = 33
+// let tshirtNumber = String(score);   
+// console.log(typeof tshirtNumber);   
+// console.log(tshirtNumber);
+
+
+// **OUTPUT**
+
+// 33 => 33  typeOf => String
+//02 => 2
+//92.87 
+
+/* ********conversion of String to Number ********** */
+
+// let playerName = "33acc"
+// let convertPlayerName = Number(playerName)
+// console.log(convertPlayerName);        
+
+ //output
+ 
+ // "33" => 33
+
+//  is NAN-(not a number) but sachin is converted & typeof is Number
+
+
+/* ********conversion of String , Number to Boolean ********** */
+
+let isLoggedIn = 0
+
+let convertInboolean = Boolean(isLoggedIn);
+
+console.log(convertInboolean);
+
+//output 
+
+// "hitesh" => true
+// "" => false
+// 0 => false , 1 => true
+
